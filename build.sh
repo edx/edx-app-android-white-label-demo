3c88adc5fd80d9e34c0132a86e7cdd82a0416828
Brian Guertin <bguertin@edx.org>
2016-07-11 15:12:10 -0400

    Replace logo, launch background

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 git submodule init
 git submodule update
 cd edx-app-android
-git checkout bguertin/whitelabel
-git pull
 echo "edx.dir = '../../config'" > VideoLocker/edx.properties 
 echo "APPLICATION_ID=com.example.whitelabelx" > VideoLocker/gradle.properties
 echo "RES_DIR=../../res" >> VideoLocker/gradle.properties
